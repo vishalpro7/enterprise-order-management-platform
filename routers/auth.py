b7594317ea462ac47ad1365c8be75e154144e394
Vishal <vishalsuji0711@gmail.com>
2026-06-17 13:02:25 +0530

    Added Authentication route (get) to the project and it connects the created table

--- routers/auth.py
+++ routers/auth.py
@@ -7,3 +7,31 @@ from models.user_model import User
 from schemas.user_schema import UserCreate
 from schemas.user_schema import UserResponse 
 
+router = APIRouter(
+    prefix = "/auth",
+    tags = ["Authentication"]
+)
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    
+    finally:
+        db.close()
+
+
+@router.post("/register", response_model = UserResponse)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        name = user.name,
+        email = user.email,
+        password = user.password,
+        role = user.role
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
