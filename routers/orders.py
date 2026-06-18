8359341ed8f84343b503f79f39c026649728111d
Vishal <vishalsuji0711@gmail.com>
2026-06-18 14:51:01 +0530

    Added Post Method for Orders and its working

--- routers/orders.py
+++ routers/orders.py
@@ -1,10 +1,44 @@
 from fastapi import APIRouter
+from fastapi import Depends
+
+from sqlalchemy.orm import Session
+
+from database.db import SessionLocal
+
+from models.order_model import Order
+from models.order_item_model import OrderItem
+from models.product_model import Product
+
+from schemas.order_schema import OrderCreate
+from schemas.order_schema import OrderResponse
+
+
+
 
 router = APIRouter(
     prefix = "/orders",
     tags = ["Orders"]
 )
 
-@router.get("/")
-def test_orders():
-    return {"message": "Orders Working"}
+def get_db():
+
+    db = SessionLocal()
+
+    try: 
+        yield db
+    
+    finally:
+
+        db.close()
+
+
+@router.post("/", response_model = OrderResponse)
+def create_order(
+    order : OrderCreate,
+    db : Session = Depends(get_db)
+):
+    return {
+        "id" : 1,
+        "user_id" : 2,
+        "total_amount" : 100
+    }
